f755787a5d85c4164d3c5574b10e2fe09f4c68d1
lawrencenuu <107622233+lawrencenuu@users.noreply.github.com>
2026-09-17 12:58:15 +0700

    addition on the solution of problem 86

--- cscms_me/cscms_problem86.py
+++ cscms_me/cscms_problem86.py
@@ -15,3 +15,29 @@ def multiply(a, b):
 num1 = int(input())
 num2 = int(input())
 print(multiply(num1,num2))
+
+# This will be for huge inputs
+# def multiply(a, b):
+#     # Base case
+#     if a == 0 or b == 0:
+#         return 0
+
+#     # Handle negative numbers
+#     if b < 0:
+#         return -multiply(a, -b)
+
+#     # If b is even:
+#     # a × b = (a × (b/2)) × 2
+#     if b % 2 == 0:
+#         half = multiply(a, b // 2)
+#         return half + half
+
+#     # If b is odd:
+#     # a × b = a + (a × (b-1))
+#     return a + multiply(a, b - 1)
+
+
+# num1 = int(input())
+# num2 = int(input())
+
+# print(multiply(num1, num2))
